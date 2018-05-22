e408b3e28d8e25557b1132168e1cbff6825a1c18
vadymchepay <vadymbychkov@gmail.com>
2018-05-22 23:23:57 +0300

    new idea

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,53 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
-  });
+});
 
+//
+//$(function () {
+//    // Init ScrollMagic
+//    var controller = new ScrollMagic.Controller();
+//
+//    // pin the intro
+//    var pinIntroScene = new ScrollMagic.Scene({
+//            triggerElement: '.intro_content',
+//            triggerHook: 0,
+//            duration: '100%'
+//        })
+//        .setPin('.intro_content', {
+//            pushFollowers: false
+//        })
+//        .addTo(controller);
+//
+//    // pin again
+//    var pinIntroScene2 = new ScrollMagic.Scene({
+//            triggerElement: '#project01',
+//            triggerHook: 0.4
+//        })
+//        .setPin('#intro', {
+//            pushFollowers: false
+//        })
+//        .addTo(controller);
+//});
 
-$(function () {
-            var controller = new ScrollMagic.Controller();
-            // define movement of panels
-            var wipeAnimation = new TimelineMax()
-            .fromTo("section.panel.turqoise", 1, {y: "0%"}, {y: "-100%", ease: Linear.easeNone}) // in from left
-            .fromTo("section.panel.green", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}); // in from right
-           
-            // create scene to pin and link animation
-            new ScrollMagic.Scene({
-                    triggerElement: "#pinContainer"
-                    , triggerHook: "onLeave"
-                    , duration: "300%"
-                }).setPin("#pinContainer").setTween(wipeAnimation)
-                .addTo(controller);
+$(window).scroll(function () {
+    var introPos = $(this).scrollTop();
+    var coverBottom = $('.intro_cover').position().top + $('.intro_cover').innerHeight();
+    var trigger_1 = false;
+
+    $('.intro_cover').css({
+        'top': -introPos + 'px'
+    });
+    if (coverBottom <= 0) {
+        trigger_1 = true;
+    };
+    if (trigger_1) {
+        var aboutPos = $('.about').offset().top;
+        $('.about').css({
+            'position' : 'relative',
+            'top': aboutPos-introPos
         });
+    }
+
+    console.log(trigger_1);
+    console.log(aboutPos);
+});
