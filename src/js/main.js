86d5baccbada96100e2b7371a970f16502c8ade2
vadymchepay <vadymbychkov@gmail.com>
2018-05-22 16:59:20 +0300

    second step

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,20 @@
-//document.addEventListener('DOMContentLoaded', function() {
-//    var elems = document.querySelectorAll('.sidenav');
-//    var instances = M.Sidenav.init(elems, options);
-//  });
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
 
-//require.config({
-//    baseUrl: 'js',
-//    paths: {
-//        TweenMax: 'lib/greensock/TweenMax',
-//        TimelineMax: 'lib/greensock/TimelineMax',
-//        jquery: 'lib/jquery.min',
-//        ScrollMagic: 'jquery.scrollmagic',
-//        "ScrollMagic.debug": 'jquery.scrollmagic.debug'
-//    }
-//});
 
-//$(function () {
-//            var controller = new ScrollMagic.Controller();
-//            // define movement of panels
-//            var wipeAnimation = new TimelineMax().fromTo("section.panel.turqoise", 1, {
-//                    x: "-100%"
-//                }, {
-//                    x: "0%"
-//                    , ease: Linear.easeNone
-//                }) // in from left
-//                .fromTo("section.panel.green", 1, {
-//                    x: "100%"
-//                }, {
-//                    x: "0%"
-//                    , ease: Linear.easeNone
-//                }) // in from right
-//                .fromTo("section.panel.bordeaux", 1, {
-//                    y: "-100%"
-//                }, {
-//                    y: "0%"
-//                    , ease: Linear.easeNone
-//                }); // in from top
-//            // create scene to pin and link animation
-//            new ScrollMagic.Scene({
-//                    triggerElement: "#pinContainer"
-//                    , triggerHook: "onLeave"
-//                    , duration: "300%"
-//                }).setPin("#pinContainer").setTween(wipeAnimation).addIndicators() // add indicators (requires plugin)
-//                .addTo(controller);
-//        });
+$(function () {
+            var controller = new ScrollMagic.Controller();
+            // define movement of panels
+            var wipeAnimation = new TimelineMax()
+            .fromTo("section.panel.turqoise", 1, {y: "0%"}, {y: "-100%", ease: Linear.easeNone}) // in from left
+            .fromTo("section.panel.green", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}); // in from right
+           
+            // create scene to pin and link animation
+            new ScrollMagic.Scene({
+                    triggerElement: "#pinContainer"
+                    , triggerHook: "onLeave"
+                    , duration: "300%"
+                }).setPin("#pinContainer").setTween(wipeAnimation)
+                .addTo(controller);
+        });
